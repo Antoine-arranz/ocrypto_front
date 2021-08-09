3006f910810a45061eb2e35e0637d89771bc6265
aarranz <aarranz@asd-int.com>
2021-08-09 13:27:27 +0200

    merge dev

--- src/containers/App.ts
+++ src/containers/App.ts
@@ -1,16 +1,5 @@
 import { connect } from "react-redux";
 import App from "../views/App";
-<<<<<<< HEAD
-import { findRecipeFromSlug } from "../reducers/recipes";
-
-const mapState = (state: any, ownProps: any): any => {
-  return {
-
-  };
-};
-
-export default connect(mapState)(App);
-=======
 import selectors from "../store/selectors";
 const mapState = (state: any) => {
   return {
@@ -19,4 +8,3 @@ const mapState = (state: any) => {
   };
 };
 export default connect(mapState, null)(App);
->>>>>>> dev
